1559b884950c7feec252ebedb3690916acce0a6a
Hanyel Chamon <hanyel.chamon@gmail.com>
2023-03-30 14:44:25 -0300

    feat: create Header component

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,7 +1,12 @@
 import * as S from './styles'
+import { Header } from '@components'
 
 import React from 'react'
 
 export const Groups: React.FC = () => {
-  return <S.Container></S.Container>
+  return (
+    <S.Container>
+      <Header />
+    </S.Container>
+  )
 }
