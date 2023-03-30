1617c2847aac79b4cfb3e237815ee61703d644ad
Hanyel Chamon <hanyel.chamon@gmail.com>
2023-03-30 15:04:19 -0300

    feat(header): add back button to header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,19 @@
 import * as S from './styles'
 import logoImg from '@assets/logo.png'
 
-export function Header() {
+type Props = {
+  showBackButton?: boolean
+}
+
+export function Header({ showBackButton = false }: Props) {
   return (
     <S.Container>
+      {showBackButton && (
+        <S.BackButton>
+          <S.BackIcon />
+        </S.BackButton>
+      )}
+
       <S.Logo source={logoImg} />
     </S.Container>
   )
--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,4 +1,5 @@
 import * as S from './styles'
+
 import { Header } from '@components'
 
 import React from 'react'
