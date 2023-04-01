91e9ec0342ee859dec9fa012314d6d5fe69430a0
Hanyel Chamon <hanyel.chamon@gmail.com>
2023-04-01 09:41:48 -0300

    feat(components|input): change placeholder color

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,7 +1,10 @@
 import * as S from './styles'
 
 import { TextInputProps } from 'react-native'
+import { useTheme } from 'styled-components/native'
 
 export function Input(props: TextInputProps) {
-  return <S.Container {...props} />
+  const theme = useTheme()
+
+  return <S.Container placeholderTextColor={theme.COLORS.GRAY_300} {...props} />
 }
