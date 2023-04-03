b58389fafc594836116814361fdba904e5a40678
Hanyel Chamon <hanyel.chamon@gmail.com>
2023-04-03 15:58:33 -0300

    fix: wrap input into fowardRef

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,10 +1,21 @@
 import * as S from './styles'
 
-import { TextInputProps } from 'react-native'
+import { TextInput, TextInputProps } from 'react-native'
 import { useTheme } from 'styled-components/native'
+import { forwardRef } from 'react'
 
-export function Input(props: TextInputProps) {
-  const theme = useTheme()
+export const Input = forwardRef<TextInput, TextInputProps>(
+  (props: TextInputProps, ref) => {
+    const theme = useTheme()
 
-  return <S.Container placeholderTextColor={theme.COLORS.GRAY_300} {...props} />
-}
+    return (
+      <S.Container
+        ref={ref}
+        placeholderTextColor={theme.COLORS.GRAY_300}
+        {...props}
+      />
+    )
+  },
+)
+
+Input.displayName = 'Input'
